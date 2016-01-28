e6d3884c14153d50fa54e239252a3cb652b3823b
emasill <marko.silla@ericsson.com>
2016-01-28 15:51:43 +0100

    script

--- pmbus/dev.sh
+++ pmbus/dev.sh
@@ -6,19 +6,14 @@ echo "PULL"
 echo "BUILD"
 make clean
 make
-#sudo make -C /lib/modules/`uname -r`/build M=$PWD modules
-#sudo make -C /lib/modules/`uname -r`/build M=$PWD pmbus_core.ko
-#sudo make -C /lib/modules/`uname -r`/build M=$PWD pmbus.ko
-#sudo make -C /lib/modules/`uname -r`/build M=$PWD zl6100.ko
-#sudo make -C /lib/modules/`uname -r`/build M=$PWD modules_install
 
 echo "COPY MODULES"
-cp zl6100.ko /lib/modules/`uname -r`/
+cp bmr46x.ko /lib/modules/`uname -r`/
 cp pmbus.ko /lib/modules/`uname -r`/
 cp pmbus_core.ko /lib/modules/`uname -r`/
 
-#echo "REMOVE MODULES"
-sudo rmmod zl6100.ko
+echo "REMOVE MODULES"
+sudo rmmod bmr46x.ko
 sudo rmmod pmbus.ko
 sudo rmmod pmbus_core.ko
 sudo modprobe -r hwmon
@@ -27,9 +22,7 @@ echo "ADD MODULES"
 sudo modprobe hwmon
 sudo modprobe pmbus_core
 sudo modprobe pmbus
-sudo modprobe zl6100 delay=30000
-#sudo insmod pmbus_core.ko
-#sudo insmod pmbus.ko
-#sudo insmod zl6100.ko
+#default
+sudo modprobe bmr46x delay=1000
 
 echo "FINISHED"
